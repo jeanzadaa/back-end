289795ce17c899333d579743a0dbdb82b3036253
jeanzadaa <ferreira.pinto.jean@escola.pr.gov.br>
2026-03-09 08:55:25 -0300

    Create cont03.py

---
+++ conteudo/cont03.py
@@ -0,0 +1,10 @@
+n= input ('Digite algo')
+print(n.isnumeric()) #para perguntar se é um número
+print (n.isalpha()) #para perguntar se é letra
+print (n.isalnum()) #para perguntar se é letra ou número
+print (n.isupper()) #para perguntar se é só com letras maiúsculas
+print (n.isdecimal()) #para perguntar se é decimal
+print (n.isdigit()) #para perguntar se é um digito
+print (n.islower()) #para perguntar se é apenas letras minúsculas
+print (n.isidentifier()) #para perguntar se é um identificador válido
+print (n.isspace()) #para verificar se tem espaço em branco
