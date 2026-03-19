187e369a5d965f264a57d12b7c1bf74eddce49fb
jeanzadaa <ferreira.pinto.jean@escola.pr.gov.br>
2026-03-19 08:10:14 -0300

    Create ex7.py

---
+++ exercícios/ex7.py
@@ -0,0 +1,3 @@
+n1= int (input('Digite um número'))
+print (' o número digitado foi {} sendo o seu dobro {}, seu triplo {} e a raiz quadrada {:.2f}'. format(n1,n1*2,n1*3,n1*0.5))
+# para calcular a raiz quadrada posso utilizar a função pow(n**0.5)
