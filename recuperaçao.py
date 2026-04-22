3c2873763629bcff157b9ea3c00d023b4299cd27
jeanzadaa <ferreira.pinto.jean@escola.pr.gov.br>
2026-04-22 11:39:33 -0300

    Create recuperaçao.py

---
+++ recuperaçao.py
@@ -0,0 +1,21 @@
+1-
+import random
+numero_aleatorio = random.randint(1, 25 )
+print(f"Número aleatório gerado: {numero_aleatorio}")
+
+2-
+ceil - Arredonda para cima 
+
+trunc- Trunca (corta) o número 
+
+sqrt - Calcula a raiz quadrada de x
+
+3-
+km = float(input("Km percorridos: "))
+dias = int(input("Dias alugados: "))
+total = (dias * 47) + (km * 0.25)
+print(f"\nTotal a pagar: R$ {total:.2f}")
+
+4-
+salario = float(input("Salário atual: R$ "))
+print(f"Novo salário com 12% de aumento: R$ {salario * 1.12:.2f}")
